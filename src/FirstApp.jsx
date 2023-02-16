045b45cfbee5482e89be9b9d5f94d57435590f90
Alejandro <frozmiz@live.com>
2023-02-16 22:11:05 +0100

    Initial commit

---
+++ src/FirstApp.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Fragment } from "react";
+
+
+const FirstApp = () => {
+  return(
+    <Fragment>
+        <h2>Esta es mi primera app con vite + React</h2>
+        <p> Papasito que velosss que va esta weadaaaaa!</p>
+    </Fragment>
+    
+  ) 
+};
+
+export default FirstApp;
