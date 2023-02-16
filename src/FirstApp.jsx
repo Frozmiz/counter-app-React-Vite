a48a0f48e045ce5447d6f56a3257bdb48c5e30ae
Alejandro <frozmiz@live.com>
2023-02-17 00:14:53 +0100

    proptypes and defaultprops

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,16 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Fragment } from "react";
+import PropTypes from "prop-types";
 
-
-const FirstApp = () => {
-  return(
+const FirstApp = ({ title, numero }) => {
+  return (
     <Fragment>
-        <h2>Esta es mi primera app con vite + React</h2>
-        <p> Papasito que velosss que va esta weadaaaaa!</p>
+      <h2>{ title }</h2>
+      <h2>Esta es mi primera app con vite + React</h2>
+      <p> Papasito que velosss que va esta weadaaaaa!</p>
+      <h4> { numero }</h4>
     </Fragment>
-    
-  ) 
+  );
+};
+
+FirstApp.propTypes = {
+  title: PropTypes.string.isRequired,
+  numero: PropTypes.number
+};
+
+FirstApp.defaultProps = {
+  title: "Hola me llamo hola",
+  numero: 23
 };
 
 export default FirstApp;
